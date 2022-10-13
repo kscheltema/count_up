322fd56a5e03a88a5467ea39c078edefb6144a3b
kscheltema <k87scheltema@outlook.com>
2022-10-13 16:57:59 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,24 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [sec, setSec] = React.useState(0);
+  const [min, setMin] = React.useState(0);
+  const [hour, setHour] = React.useState(0);
+  const [day, setDay] = React.useState(0);
+
+  const timeHandler = () => {
+    setSec(sec + 1);
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <p>{sec}</p>
+        <button onClick={timeHandler}>START</button>
+        <button onClick={timeHandler}>STOP</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
