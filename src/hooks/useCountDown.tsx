5cd4bab66cbe551d5f140c1e996a5165d53d3240
kscheltema <k87scheltema@outlook.com>
2022-10-30 20:48:55 +0200

    useCountDown WIP

--- src/hooks/useCountDown.tsx
+++ src/hooks/useCountDown.tsx
@@ -1,18 +1,28 @@
 import * as React from "react";
 
+const getReturnValues = (countDown: number) => {
+  const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+  return [days, hours, minutes, seconds];
+};
+
 const useCountDown = (targetDate: string) => {
   const countDownDate: Date = new Date(targetDate);
   const countDownTime: number = countDownDate.getTime();
 
   // Here you want you time minus the current time. (positive if future negative if time since)
   const [countDown, setCountDown] = React.useState<number>(
-    countDownDate - countDownTime
+    countDownTime - Date.now()
   );
 
   React.useEffect(() => {
     const interval = setInterval(() => {
       // here you want the same as above because it will change every second.
-      setCountDown(countDownDate - countDownTime);
+      setCountDown(countDownTime - Date.now());
     }, 1000);
     return () => clearInterval(interval);
   }, [countDownTime, countDownDate]);
@@ -22,14 +32,5 @@ const useCountDown = (targetDate: string) => {
 };
 // here you have put an "external function" within your current function
 // put this at the top for easier readability
-const getReturnValues = (countDown) => {
-  const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  );
-  const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
-  return [days, hours, minutes, seconds];
-};
 
 export { useCountDown };
