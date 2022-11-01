dde825b80f35c05905b1ea8e51b1b82c188cd60d
kscheltema <k87scheltema@outlook.com>
2022-11-01 19:17:26 +0200

    count down timer user input pending

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./App.css";
 import CountDownTimer from "./components/CountDownTimer";
 
-function App(targetDate: string) {
+function App() {
   return (
     <div className="App">
-      <CountDownTimer targetDate={targetDate} />
+      <CountDownTimer targetDate={"2023-01-01 23:59:59"} />
     </div>
   );
 }
